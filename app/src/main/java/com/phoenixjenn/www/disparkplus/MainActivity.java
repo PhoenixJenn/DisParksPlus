2f9ff60c62c5da444976a7bd03b5004f074f03e6
Jenn <jnnlee@amazon.com>
2018-07-15 08:20:04 -0400

    test

--- app/src/main/java/com/phoenixjenn/www/disparkplus/MainActivity.java
+++ app/src/main/java/com/phoenixjenn/www/disparkplus/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//ToDo: testing file change
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
